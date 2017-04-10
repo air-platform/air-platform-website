81096d671e4f580de8e3bfe4d4ccb8900addeff7
__无邪_ <fqah@qq.com>
2017-04-10 18:18:35 +0800

    add jet and taxi

---
+++ src/app/components/airtaxi/airtaxi.controller.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Otherplayer on 2017/4/10.
+ */
+/**
+ * Created by Otherplayer on 16/7/21.
+ */
+(function () {
+    'use strict';
+
+    angular.module('airsc').controller('taxiController', taxiController);
+
+    /** @ngInject */
+    function taxiController($scope,iotUtil) {
+
+
+    }
+
+})();
