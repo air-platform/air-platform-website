59945d3785086c71c515b045baf77f1eeb0d2e4b
__无邪_ <fqah@qq.com>
2017-04-10 17:51:29 +0800

    add controller

--- src/app/components/about/about.controller.js
+++ src/app/components/about/about.controller.js
@@ -4,16 +4,16 @@
 (function () {
     'use strict';
 
-    angular.module('airsc').controller('mainController', mainController);
+    angular.module('airsc').controller('aboutController', aboutController);
 
     /** @ngInject */
-    function mainController($scope,iotUtil) {
+    function aboutController($scope,iotUtil) {
         /* jshint validthis: true */
         var vm = this;
 
         $scope.title = "Is in main.controller.js";
 
-        $scope.about = 'about';
+        $scope.about2 = 'aboutsss';
 
         console.log(iotUtil.uuid());
     }
