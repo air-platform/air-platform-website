dc02d9cb4d0ca97ebe52bb6eda7adcacab6db407
__无邪_ <fqah@qq.com>
2017-04-10 18:46:52 +0800

    add module

---
+++ src/app/components/airtrain/airtrain.controller.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Otherplayer on 2017/4/10.
+ */
+/**
+ * Created by Otherplayer on 16/7/21.
+ */
+(function () {
+    'use strict';
+
+    angular.module('airsc').controller('trainController', trainController);
+
+    /** @ngInject */
+    function trainController($scope,iotUtil) {
+
+
+    }
+
+})();
