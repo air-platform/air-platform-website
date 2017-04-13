253fadd25c805e5ace0ccd5927944450fc137842
__无邪_ <fqah@qq.com>
2017-04-13 11:23:27 +0800

    fix register

--- src/app/components/login/register.controller.js
+++ src/app/components/login/register.controller.js
@@ -41,15 +41,16 @@
 
         function getAuthcode() {
             $scope.authcodediabled = true;
-            countDown = 60;
-            timer = $interval($scope.upd_count ,1000,60);
             myApp.showIndicator();
             NetworkService.post('account/verification?mobile=' + $scope.mobile,null,function (res) {
+                countDown = 120;
+                timer = $interval($scope.upd_count ,1000,120);
                 myApp.showPreloader('验证码发送成功，请注意查收');
                 setTimeout(function () {
                     myApp.hidePreloader();
                 }, 2000);
             },function (err) {
+                $scope.authcodediabled = false;
                 var errDesc = err.statusText;
                 myApp.hideIndicator();
                 myApp.alert('操作失败！' + errDesc, null);
