cb492a243d566a1cfb45edbc5ab70f35020dd0e0
phinney <phinney001@qq.com>
2017-04-13 21:33:58 +0800

    update datepicker

--- src/app/components/airjet/airjet.controller.js
+++ src/app/components/airjet/airjet.controller.js
@@ -12,6 +12,7 @@
         var formData = page.activePage.query;
         $scope.submit = submit;
         $scope.addCard = addCard;
+        $scope.removeCard = removeCard;
         $scope.datepicter = datepicter;
 
         $scope.imgSrc = [
@@ -105,27 +106,26 @@
             if(!$scope.travelPlanList || !$scope.travelPlanList.length) {
                 $scope.travelPlanList = [1];
             } else  {
-                $scope.travelPlanList.push(Number($scope.travelPlanList[$scope.travelPlanList.length - 1]) + 1);
+                for(var i = 1; i < 100; i++) {
+                    if($scope.travelPlanList.indexOf(i) === -1) {
+                        $scope.travelPlanList.push(i);
+                        return;
+                    }
+                }
             }
         };
 
-        function datepicter(){
-            var calendar = new datePicker();
-            calendar.init({
-                'trigger': '.datepicter', /*按钮选择器，用于触发弹出插件*/
-                'type': 'datetime',/*模式：date日期；datetime日期时间；time时间；ym年月；*/
-                'minDate':'1900-1-1',/*最小日期*/
-                'maxDate':'2100-12-31',/*最大日期*/
-                'onSubmit':function(){/*确认时触发事件*/
-                    if(!$scope.formData) {
-                        $scope.formData = {};
-                    }
-                    $scope.formData.time = calendar.value;
-                    console.log($scope.formData)
-                },
-                'onClose':function(){/*取消时触发事件*/
+        function removeCard(item) {
+            $scope.travelPlanList.splice($scope.travelPlanList.indexOf(item), 1)
+        };
+
+        function datepicter(name, index){
+            var calendar = myApp.calendar({
+                input: '.' + name + index,
+                onDayClick: function() {
+                    calendar.close();
                 }
-            });
+            });    
         }
     }
 
---
+++ src/app/components/airjet/travel.info.controller.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Otherplayer on 16/7/21.
+ */
+(function () {
+    'use strict';
+
+    angular.module('airsc').controller('travelInfoController', travelInfoController);
+
+    /** @ngInject */
+    function travelInfoController($scope,iotUtil) {
+
+
+    }
+
+})();
