fb171c4ee073ef6a3c958162d53b93308e735413
__无邪_ <fqah@qq.com>
2017-04-14 14:41:53 +0800

    --

--- src/app/components/order/order.controller.js
+++ src/app/components/order/order.controller.js
@@ -16,10 +16,12 @@
             [{no:'订单编号: 993028364001',date:'订单时间: 2017年10月25日',url:'http://p2.ifengimg.com/a/2017_15/f5455f5a3ba44fc_size18_w490_h271.jpg',name:'固定翼私照',subname:'海南航空学校',price:'￥25万元',status:3}]
         ];
 
-
-
+        $scope.gotoOrderDetail = gotoOrderDetail;
         $scope.showSelectViewAction = showSelectViewAction;
-        
+
+        function gotoOrderDetail(itemIndex,index) {
+            mainView.router.loadPage('app/components/order/orderdetail.html');
+        }
         function showSelectViewAction() {
             console.log('---');
         }
