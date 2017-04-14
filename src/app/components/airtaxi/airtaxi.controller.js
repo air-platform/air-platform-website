2633a84d96e765962761cdc96643c65c933dfd08
__无邪_ <fqah@qq.com>
2017-04-14 13:19:40 +0800

    add taxi

---
+++ src/app/components/airtaxi/airtaxi.controller.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Otherplayer on 16/7/21.
+ */
+(function () {
+    'use strict';
+
+    angular.module('airsc').controller('taxiController', taxiController);
+
+    /** @ngInject */
+    function taxiController($scope,iotUtil) {
+        /* jshint validthis: true */
+        var vm = this;
+
+
+    }
+
+})();
