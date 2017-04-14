d1789175aa2b8b7173df19227d077f43512c456f
__无邪_ <fqah@qq.com>
2017-04-14 13:03:19 +0800

    订单查询界面

--- src/app/components/order/order.controller.js
+++ src/app/components/order/order.controller.js
@@ -1,7 +1,4 @@
 /**
- * Created by Otherplayer on 2017/4/10.
- */
-/**
  * Created by Otherplayer on 16/7/21.
  */
 (function () {
@@ -11,7 +8,6 @@
 
     /** @ngInject */
     function orderController($scope,NetworkService,$interval,iotUtil) {
-
 
         $scope.items = [
             [{no:'订单编号: 993028364001',date:'订单时间: 2017年10月25日',url:'http://p2.ifengimg.com/a/2017_15/f5455f5a3ba44fc_size18_w490_h271.jpg',name:'固定翼私照',subname:'海南航空学校',price:'￥25万元',status:0}],
@@ -23,12 +19,9 @@
 
 
         $scope.showSelectViewAction = showSelectViewAction;
-
-
-        
         
         function showSelectViewAction() {
-            
+            console.log('---');
         }
 
     }
