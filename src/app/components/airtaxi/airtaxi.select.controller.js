6ceec9c911f7d886152f6c97258443712f8bd622
donghaitao <donghaitao@qq.com>
2017-04-15 14:11:32 +0800

    select update

---
+++ src/app/components/airtaxi/airtaxi.select.controller.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+
+    angular.module('airsc').controller('airtaxiSelectController', airtaxiSelectController);
+
+    /** @ngInject */
+    function airtaxiSelectController($scope, iotUtil) {
+    	$scope.airSelectList = [{
+            imgSrc: './../../../assets/images/hotair.png',
+            titleText: '首都直升机B-7186',
+            disc: '航班号B-7651',
+            time: '40',
+            price: '1000'
+        }, {
+            imgSrc: './../../../assets/images/hotair.png',
+            titleText: '首都直升机E-135',
+            disc: '送10分钟地面直升机静态摄影',
+            time: '40',
+            price: '2000' 
+        }, {
+        	imgSrc: './../../../assets/images/hotair.png',
+            titleText: '固定翼单发商照(不含仪表)',
+            disc: '与机长互动沟通调整飞行高度和路线',
+            time: '40',
+            price: '3000'
+        }];
+    }
+})();
