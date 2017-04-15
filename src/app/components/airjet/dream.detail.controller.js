4b6ef8673c9ad7e69950de2c51be097d90fd30f1
phinney <phinney001@qq.com>
2017-04-16 00:03:44 +0800

    update travel data

--- src/app/components/airjet/dream.detail.controller.js
+++ src/app/components/airjet/dream.detail.controller.js
@@ -8,7 +8,19 @@
 
     /** @ngInject */
     function dreamDetailController($scope,iotUtil) {
-
+        $scope.detailData = {
+            order: 993028364001,
+            model: '金鹿航空 BBJ-002',
+            date: '2017年04月07日 周六',
+            startCity: '香港',
+            startTime: '13:00',
+            startLocal: '首都机场T2航站楼',
+            endCity: '北京',
+            endTime: '16:30',
+            endLocal: '美兰机场',
+            passenger: '3',
+            remark: '无'
+        }
 
     }
 
--- src/app/components/airjet/travel.detail.controller.js
+++ src/app/components/airjet/travel.detail.controller.js
@@ -8,7 +8,10 @@
 
     /** @ngInject */
     function travelDetailController($scope,iotUtil) {
-
+        var queryData = myApp.views[0].activePage.query;
+        if(queryData.info) {
+            $scope.userInfo = JSON.parse(queryData.info);
+        }
 
     }
 
