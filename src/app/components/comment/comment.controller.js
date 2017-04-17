b8536b79a05deac2973aedc0f9ec7a8474c398d8
__无邪_ <fqah@qq.com>
2017-04-17 18:25:11 +0800

    add comment controller

---
+++ src/app/components/comment/comment.controller.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Otherplayer on 16/7/21.
+ */
+(function () {
+    'use strict';
+
+    angular.module('airsc').controller('CommentController', CommentController);
+
+    /** @ngInject */
+    function CommentController($scope, $rootScope, iotUtil, i18n) {
+
+
+    }
+
+})();
