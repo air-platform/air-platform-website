19b5ce8718a341754f4e1830ff571f5f3f87b20c
__无邪_ <fqah@qq.com>
2017-04-18 14:29:54 +0800

    add reset password

--- src/app/components/setting/password.controller.js
+++ src/app/components/setting/password.controller.js
@@ -9,6 +9,26 @@
     /** @ngInject */
     function passwordController($scope, $rootScope, iotUtil, i18n) {
 
+        $scope.oldPassword = '';
+        $scope.newPassword = '';
+        $scope.resetAction = resetAction;
+
+
+        function resetAction() {
+            myApp.showIndicator();
+
+            NetworkService.post('account/password',{oldPassword:$scope.oldPassword,newPassword:$scope.newPassword},function (res) {
+                myApp.hideIndicator();
+                myApp.alert('重置密码成功！', 'Air Community', function () {
+                    mainView.router.back();
+                });
+            },function (err) {
+                var errDesc = err.statusText;
+                myApp.hideIndicator();
+                myApp.alert('操作失败！' + errDesc, null);
+            });
+
+        }
 
     }
 
