987a6a8fe68daf4e407670aa1e3560c48d7c362b
__无邪_ <fqah@qq.com>
2017-04-19 14:16:52 +0800

    获取评论

--- src/app/components/setting/setting.controller.js
+++ src/app/components/setting/setting.controller.js
@@ -7,7 +7,7 @@
     angular.module('airsc').controller('settingController', settingController);
 
     /** @ngInject */
-    function settingController($scope, $rootScope, iotUtil, i18n) {
+    function settingController($scope,CommentServer, $rootScope, iotUtil, i18n) {
 
 
         $scope.gotoDetail = gotoDetail;
@@ -21,12 +21,35 @@
 
         /** -评论- **/
         $scope.loading = false;
+        $scope.comments = [];
+        var CCPage = 1;
         // 注册'infinite'事件处理函数
         $$('.infinite-scroll').on('infinite', function () {
             if ($scope.loading)return;
             $scope.loading = true;
-            console.log('----');
         });
+
+        getLatestFirstComment();
+
+        function getLatestFirstComment(productId) {
+            CommentServer.getLatestComment(productId,function (res) {
+
+            });
+        }
+        function getComments(productId,cp) {
+            CommentServer.getComments(productId,cp,function (res) {
+                var data = res.data.content;
+                if (data && data.length > 0){
+                    //delete first
+                    data.pop();
+                }
+                $scope.comments.concat(data);
+                $scope.loading = false;
+            },function (err) {
+                $scope.loading = false;
+            });
+        }
+
         /** -end- **/
 
 
