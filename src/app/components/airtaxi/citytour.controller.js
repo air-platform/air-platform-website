4a710514a2c5746cf6717ea5a09ffa1c12f156d8
Gao Yan <gaoybest@gmail.com>
2017-04-20 14:01:55 +0800

    integrated city tour list

--- src/app/components/airtaxi/citytour.controller.js
+++ src/app/components/airtaxi/citytour.controller.js
@@ -10,7 +10,7 @@
     angular.module('airsc').controller('citytourController', citytourController);
 
     /** @ngInject */
-    function citytourController($scope, NotificationService) {
+    function citytourController($scope, NotificationService, NetworkService) {
       var citys = ['北京', '桂林', '海南', '宁波'];
       var today = new Date();
 
@@ -20,33 +20,32 @@
         $scope.city = queryData.city || citys[0];
         angular.element('#citytour-title').text($scope.city + '观光');
         $scope.tourdate = convertDate(today);
-        $scope.sitesList = [{
-          pic: 'assets/images/city.png',
-          title: '八达岭长城观光',
-          info: '近距离低空俯览八达岭长城'
-        },{
-          pic: 'assets/images/city.png',
-          title: '八达岭长城观光',
-          info: '近距离低空俯览八达岭长城'
-        },{
-          pic: 'assets/images/city.png',
-          title: '八达岭长城观光',
-          info: '近距离低空俯览八达岭长城'
-        }];
+        $scope.sitesList = [];
       }
-
+      var loadData = function(page) {
+        NetworkService.get("tours", {city: $scope.city, page: page}, function(res) {
+          var data = res.data;
+          $scope.sitesList = $scope.sitesList || [];
+          $scope.sitesList = $scope.sitesList.concat(data.content);
+        }, function(res) {
+          NotificationService.alert.error(res.statusText, null);
+        });
+      }
       var pickerDevice = myApp.picker({
           input: '#citytour-title',
           cols: [
               {
                   textAlign: 'center',
                   values: citys,
-                  onChange: function(item, current){
-                    angular.element('#citytour-title').text(current + '观光');
-                    $scope.city = current;
-                  }
               }
-          ]
+          ],
+          onChange: function(item, current){
+            angular.element('#citytour-title').text(current + '观光');
+            $scope.city = current;
+          },
+          onOpen: function() {
+            pickerDevice.setValue(angular.element('#citytour-title').text().replace("观光", ""));
+          }
       });
 
       var calendarDateFormat = myApp.calendar({
@@ -72,13 +71,23 @@
         return yyyy + '-' + (mmChars[1]?mm:"0"+mmChars[0]) + '-' + (ddChars[1]?dd:"0"+ddChars[0]);
       }
 
-      function jumpDetail(data){
+      function jumpDetail(site){
         if(!$scope.tourdate) {
           NotificationService.alert.success('请选择出行日期', null);
           return;
         }
+        mainView.pageData = mainView.pageData || {};
+        mainView.pageData.tourdate = $scope.tourdate;
+        mainView.pageData.site = site;
+        mainView.pageData.city = $scope.city;
         mainView.router.loadPage('app/components/airtaxi/project-details.html?date=' + $scope.tourdate);
       };
+
+      $scope.$watch(function() {
+        return angular.element('#citytour-title').text().replace("观光", "");
+      }, function(oldValue, newValue) {
+        if(newValue) loadData(1);
+      });
       init();
     };
 })();
