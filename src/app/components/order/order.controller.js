211935cd26288342e6ae270f966c4c369a9b29ab
__无邪_ <fqah@qq.com>
2017-04-20 10:36:54 +0800

    f

--- src/app/components/order/order.controller.js
+++ src/app/components/order/order.controller.js
@@ -7,24 +7,22 @@
     angular.module('airsc').controller('orderListController', orderListController);
 
     /** @ngInject */
-    function orderListController($scope,OrderServer,NotificationService,iotUtil) {
-
-
-        // $scope.items = [
-        //     [{no:'订单编号: 993028364001',date:'订单时间: 2017-10-25',url:'http://p2.ifengimg.com/a/2017_15/f5455f5a3ba44fc_size18_w490_h271.jpg',name:'固定翼私照',subname:'海南航空学校',price:'￥25万元',status:0}],
-        //     [{no:'订单编号: 993028364001',date:'订单时间: 2017-10-25',url:'http://p2.ifengimg.com/a/2017_15/f5455f5a3ba44fc_size18_w490_h271.jpg',name:'固定翼私照',subname:'海南航空学校',price:'￥25万元',status:1}],
-        //     [{no:'订单编号: 993028364001',date:'订单时间: 2017-10-25',url:'http://p2.ifengimg.com/a/2017_15/f5455f5a3ba44fc_size18_w490_h271.jpg',name:'固定翼私照',subname:'海南航空学校',price:'￥25万元',status:2}],
-        //     [{no:'订单编号: 993028364001',date:'订单时间: 2017-10-25',url:'http://p2.ifengimg.com/a/2017_15/f5455f5a3ba44fc_size18_w490_h271.jpg',name:'固定翼私照',subname:'海南航空学校',price:'￥25万元',status:3}]
-        // ];
+    function orderListController($scope,OrderServer,NotificationService) {
 
         $scope.items = [[],[],[],[]];
 
-        //creationDate  orderNo
+        $scope.tabChanged = tabChanged;
         $scope.gotoOrderDetail = gotoOrderDetail;
         $scope.showSelectViewAction = showSelectViewAction;
         $scope.funAction = funAction;
         $scope.gotoCommentAction = gotoCommentAction;
 
+        function tabChanged(tabIndex) {
+            var tempData = $scope.items[tabIndex];
+            if (0 === tempData.length){
+                getDatas(tabIndex,1);
+            }
+        }
         function funAction(index,tabIndex) {
             var item = $scope.items[tabIndex][index];
             if (item.status === 'pending'){//取消订单
@@ -34,7 +32,7 @@
             }else if (item.status === 'cancelled'){//再次下单
 
             }
-                //do nothing
+            //do nothing
         }
         function gotoCommentAction(orderId) {
             mainView.router.loadPage('app/components/comment/comment.html');
@@ -73,7 +71,7 @@
         }
 
 
-        getDatas(0,1);
+        tabChanged(0);
 
     }
 
