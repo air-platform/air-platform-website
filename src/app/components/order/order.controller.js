a54898ed5eab68487f3d962b1f5f0692f48effa0
__无邪_ <fqah@qq.com>
2017-04-22 15:11:20 +0800

    done

--- src/app/components/order/order.controller.js
+++ src/app/components/order/order.controller.js
@@ -34,27 +34,12 @@
             var item = $scope.items[tabIndex][index];
 
             if (item.status === 'pending'){//取消订单
-                // gotoCommentAction(item.id,item);
                 cancelOrderAction(item.id,index,tabIndex);
             }else if (item.status === 'paid'){
 
             }else if (item.status === 'finished' && !item.commented){
                 gotoCommentAction(item.id,item);
             }
-        }
-        function deleteOrderAction(index,tabIndex) {
-            var item = $scope.items[tabIndex][index];
-            OrderServer.deleteOrder(item.id,function (res) {
-                myApp.alert('删除成功',function () {
-                    $scope.items[tabIndex].splice(index,1);
-                    $scope.$apply();
-                });
-            });
-            // myApp.confirm('确定删除订单吗',
-            //     function () {
-            //
-            //     }
-            // );
         }
 
         function gotoCommentAction(orderId,item) {
@@ -82,6 +67,7 @@
             myApp.confirm('订单取消后无法恢复', '确定取消订单吗',
                 function () {
                     OrderServer.cancelOrder(orderId,function (res) {
+                        //TODO:更新其它里面的对应订单
                         myApp.alert('取消成功',function () {
                             $scope.items[tabIndex][index].status = 'cancelled';
                             $scope.$apply();
@@ -91,6 +77,35 @@
                     });
                 }
             );
+        }
+        function deleteOrderAction(index,tabIndex) {
+            var item = $scope.items[tabIndex][index];
+            OrderServer.deleteOrder(item.id,function (res) {
+                //TODO:删除其它tab1,tab2，tab3里面的对应订单
+                myApp.alert('删除成功',function () {
+                    $scope.items[tabIndex].splice(index,1);
+                    $scope.$apply();
+                });
+            });
+            // myApp.confirm('确定删除订单吗',
+            //     function () {
+            //
+            //     }
+            // );
+        }
+        function updateStatus(orderId,status) {
+            $scope.items.forEach(function (orders,tabIndex) {
+                orders.forEach(function (order,index) {
+                    if (order.id === orderId){
+                        if (order.id === orderId){
+                            $scope.items[tabIndex][index].status = status;
+                        }
+                    }
+                });
+            });
+        }
+        function deleteOrderWithId(orderId) {
+
         }
 
 
