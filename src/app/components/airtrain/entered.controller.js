e42799cc77b054b0e3575926a92f48f31403ce4e
donghaitao <donghaitao@qq.com>
2017-04-24 14:17:54 +0800

    bug fix

--- src/app/components/airtrain/entered.controller.js
+++ src/app/components/airtrain/entered.controller.js
@@ -4,8 +4,9 @@
   angular.module('airsc').controller('enteredController', enteredController);
 
   /** @ngInject */
-  function enteredController($scope, iotUtil, i18n, NetworkService, UrlService, URL) {
+  function enteredController($scope, iotUtil, i18n, NetworkService, UrlService, URL, REGEX) {
     var queryData = myApp.views[0].activePage.query;
+
     $scope.enterObj = angular.fromJson(queryData.param);
     $scope.enterObj.license = '私照';
     $scope.enterObj.location = '北京';
@@ -13,14 +14,30 @@
 
     delete $scope.enterObj.id;
 
+    $scope.licenses = ['私照', '商照'];
+    $scope.place = ['北京', '宜昌', '安阳'];
+
+    $scope.licenseClick = function(index) {
+        $scope.enterObj.license = $scope.licenses[index];
+    }
+
+    $scope.locationClick = function(index) {
+      $scope.enterObj.location = $scope.place[index];
+    }
+
     $scope.enterBtn = function() {
       if(!$scope.enterObj.person && !$scope.enterObj.identity) {
-        myApp.alert(i18n.t('profile.check-input'), null)
+        myApp.alert(i18n.t('profile.check-input'), null);
         return;
       }
-      console.log($scope.enterObj);
+
+      if(!REGEX.IDCARD.test($scope.enterObj.identity)) {
+        myApp.alert('身份证格式不正确！', null);
+        return;
+      }
+
       NetworkService.post(UrlService.getUrl(URL.COURSEENTER), $scope.enterObj, function(res) {
-        myApp.alert('报名成功！', null)
+        myApp.alert('报名成功！', null);
         mainView.router.back();
       }, function(err) {
         myApp.alert('报名失败，' + err.statusText, null);
