129523827e644b1d561db581aa12c79dd6caec81
guankai <Rootroot1>
2017-04-27 15:44:50 +0800

    添加50个地图点

--- src/app/common/services/common/map-utils.service.js
+++ src/app/common/services/common/map-utils.service.js
@@ -19,11 +19,15 @@
       return allPoints;
     };
 
-    var drawMap = function(target, points, config) {
+    var drawMap = function(target, points, config,callback) {
+
+
       // MapService.mapPromise().then(function () {
         if (!config) config = {};
         // 百度地图API功能
         var map = new BMap.Map(target);
+
+
         // map.disableDragging();
         // map.disableScrollWheelZoom();
         // map.disableDoubleClickZoom();
@@ -38,6 +42,10 @@
         }
         if (config.curves) { // draw curve routes
           drawCurves(map, routes);
+        }
+
+        if (callback && typeof(callback) === "function") {
+            callback(map);
         }
 
         function parseSingleRoutes(points) {
@@ -200,11 +208,122 @@
       while(curves.length > 0) map.removeOverlay(curves.pop());
       if(withCurves) drawCurves(map, window.mapDrawPointRoutes);
     }
+
+
+    var drawFakePoints = function(map){
+
+        var fakePoints = '海口市西海岸会所,110.2263889,20.03611111;' +
+            '海口国兴大道海航广场,110.3402778,20.03611111;' +
+            '三亚市南山寺,109.2088889,18.29638889;' +
+            '三亚市亚太国际会议中心,109.3883333,18.290000;' +
+            '三亚市海棠湾,109.7377778,18.35194444;' +
+            '万宁市兴隆康乐园,110.2252778,18.76055556;' +
+            '陵水YOHO湾,110.0461111,18.54583333;' +
+            '琼海市博鳌男爵公馆,110.5933333,19.16416667;' +
+            '海口市新国宾酒店,110.2144444,20.0525;' +
+            '海南市南海明珠,110.1894444,20.06611111;' +
+            '海口美兰机场,110.468596,19.944221;' +
+            '五指山风景区,109.6777778,18.90916667;' +
+            '临高县临高角,109.7097222,20.00388889;' +
+            '乐东县莺歌海,108.6958333,18.51472222;' +
+            '东方市八所,108.62,19.09444444;' +
+            '保亭神玉文化中心,109.5786111,18.65472222;' +
+            '文县市铺前港,110.5755556,20.02416667;' +
+            '屯昌县木色湖,109.9811111,19.20388889;' +
+            '东方通用机场,108.68,19.13305556;' +
+            '珠海直三亚基地,109.4316667,18.30527778;' +
+            '三亚市海航旅游学院,109.4316667,18.29361111;' +
+            '三亚市凯宾斯基酒店,109.7372222,18.32861111;' +
+            '三亚市万豪酒店,109.6375,18.23194444;' +
+            '三亚市丽思卡尔顿酒店,109.6327778,18.23027778;' +
+            '三亚市红树林酒店,109.7338889,18.34111111;' +
+            '三亚市凤凰岛度假酒店,109.4991667,18.24666667;' +
+            '三亚市鹿回头洲际酒店,109.4961111,18.21277778;' +
+            '三亚市西岛,109.3622222,18.22916667;' +
+            '博鳌市男爵公馆,110.4536111,19.13916667;' +
+            '三亚市大小洞天,109.1483333,18.30916667;' +
+            '三亚市香格里拉酒店,109.7472222,18.35777778;';
+      // var routes = parseSingleRoutes(fakePoints); // return array of arrays
+      // window.mapDrawPointRoutes = routes;
+      var markerPoints = parseMarkers(parseMapPoints(fakePoints)); // return array of ["label", BMap.Point]
+      map.setViewport(_.map(markerPoints, function(p) { return p[1]; }));
+
+      addMarkers(markerPoints, { 'labelType': 'onclick'});
+
+        function parseMapPoints(fakePoints) {
+            var points = [];
+            var loc = fakePoints.split(';');
+            for (var i=0; i< loc.length;i++){
+                var point = loc[i].split(',');
+                points.push([point[0],point[1],point[2]]);
+            }
+            return points;
+        }
+
+      function parseSingleRoutes(points) {
+        var routes = [];
+        for (var i = 0; i < points.length; i += 2) {
+          var route = [points[i], points[i + 1]];
+          if (_.find(routes, function (r) {
+                return _.isEqual(r, route) || _.isEqual(r, [route[1], route[0]])
+              }) == null) {
+            routes.push(route);
+          }
+        }
+        return routes;
+      }
+
+      function addMarkers(markerPoints, config) {
+        _.each(markerPoints, function(pt) {
+          var icon = new BMap.Icon("assets/images/airtaxi/map-marker.svg",
+              new BMap.Size(20, 30));
+          var marker = new BMap.Marker(pt[1], {icon: icon});
+          if(config.labelType == 'static' ) {
+            var label = new BMap.Label(pt[0], {
+              offset: new BMap.Size(20, 5)
+            });
+            label.setStyle({
+              fontSize : "12px",
+              height: "18px"
+            });
+            marker.setLabel(label);
+          }
+          if(config.labelType == 'onclick') {
+            marker.addEventListener('click', function(e) {
+              var markers = _.each(map.getOverlays(), function(overlay) {
+                if(overlay instanceof BMap.Marker) {
+                  var label = overlay.getLabel();
+                  if(label) label.remove();
+                }
+              });
+              if(marker.getLabel() == null) {
+                var label = new BMap.Label(pt[0], {
+                  offset: new BMap.Size(20, 5)
+                });
+                label.setStyle({
+                  fontSize : "12px",
+                  height: "18px"
+                });
+                marker.setLabel(label);
+              }
+            });
+          }
+          marker.setOffset(new BMap.Size(0, -20));
+          marker.setZIndex(100);
+          map.addOverlay(marker);
+          marker.addEventListener("click", function (e) {
+            var point = e.currentTarget.point;
+          });
+        });
+      }
+    }
+
     return {
       'drawMap': drawMap,
       'markCurve': markCurve,
       'removeMarkedCurve': removeMarkedCurve,
-      'extractPoints': extractPoints
+      'extractPoints': extractPoints,
+      'drawFakePoints':drawFakePoints
     };
   }
 })();
